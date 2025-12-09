bca2219038eb25fed2a388b8d829b7ef9369c040
Thorben Denzer <denzer@atix.de>
2025-12-09 15:17:48 +0100

    feat(rex): allow AnsibleNavigatorRunner to handle REX-job playbooks
    - Introduce differentiation between "literal" and "generated" playbook inputs
    - Add RunAnsibleScript action to handle REX job playbooks
    - Update AnsibleNavigatorRunner to support the "literal" mode for playbooks

--- lib/smart_proxy_ansible_director/runners.rb
+++ lib/smart_proxy_ansible_director/runners.rb
@@ -7,7 +7,6 @@ module Proxy
       require 'smart_proxy_ansible_director/runners/ansible_navigator_runner'
       require 'smart_proxy_ansible_director/runners/podman_push_runner'
       require 'smart_proxy_ansible_director/runners/podman_pull_runner'
-      require 'smart_proxy_ansible_director/runners/ansible_script_runner'
     end
   end
 end
