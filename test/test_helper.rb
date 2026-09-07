0ff107b640eb83f3dd62367a4c90c238e4a3196d
Nadja Heitmann <nadjah@atix.de>
2026-09-07 21:46:09 +0200

    fix(test): update infrastructure for Sinatra 4
    Sinatra will check host authorization and throw 403 errors starting in
    Sinatra 4. This patch prevents that behavior.
    
    Refs:
    https://github.com/theforeman/smart-proxy/commit/c2af3d35497058fd7dc8146dcbca3adf60334b9e

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
+ENV['RACK_ENV'] = 'test'
+
 require 'test/unit'
 require 'mocha/test_unit'
 require 'rack/test'
 
 require 'smart_proxy_for_testing'
-
-ENV['RACK_ENV'] = 'test'
 
 # create log directory in our (not smart-proxy) directory
 FileUtils.mkdir_p File.dirname(Proxy::SETTINGS.log_file)
